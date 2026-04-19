96c7f8a84cc403ea41d3a3da4f3adda5d57f50c3
Eric <hayter.eric@gmail.com>
2026-04-19 16:49:53 -0400

    Add LSM to server code now instead of dictionary

--- server/main.go
+++ server/main.go
@@ -6,49 +6,86 @@ import (
 	"fmt"
 	"log/slog"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	pb "github.com/EricHayter/yakv/proto"
+	"github.com/EricHayter/yakv/server/lsm"
+	"github.com/EricHayter/yakv/server/storage_manager"
 	"google.golang.org/grpc"
 )
 
 var (
-	port     = flag.Int("port", 50051, "The server port")
-	database = make(map[string]string)
+	port = flag.Int("port", 50051, "The server port")
 )
 
 type server struct {
 	pb.UnimplementedYakvServerServer
+	lsm *lsm.LogStructuredMergeTree
 }
 
 func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
-	value, prs := database[in.GetKey()]
-	if prs {
+	value, found := s.lsm.Get(in.GetKey())
+	if found {
 		return &pb.GetResponse{Value: value, Status: pb.Status_SUCCESS}, nil
-	} else {
-		return &pb.GetResponse{Value: value, Status: pb.Status_FAILURE}, nil
 	}
+	return &pb.GetResponse{Value: "", Status: pb.Status_FAILURE}, nil
 }
 
 func (s *server) Put(ctx context.Context, in *pb.PutRequest) (*pb.PutResponse, error) {
-	database[in.GetKey()] = in.GetValue()
+	s.lsm.Put(in.GetKey(), in.GetValue())
 	return &pb.PutResponse{Status: pb.Status_SUCCESS}, nil
 }
 
 func (s *server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteResponse, error) {
-	delete(database, in.GetKey())
+	s.lsm.Delete(in.GetKey())
 	return &pb.DeleteResponse{Status: pb.Status_SUCCESS}, nil
 }
 
 func main() {
 	flag.Parse()
+
+	// Initialize storage manager
+	sm, err := storage_manager.New(100)
+	if err != nil {
+		slog.Error("failed to create storage manager", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	// Initialize LSM tree
+	lsmTree, err := lsm.New(sm)
+	if err != nil {
+		slog.Error("failed to create LSM tree", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	// Set up graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		slog.Info("shutting down gracefully...")
+		if err := lsmTree.Close(); err != nil {
+			slog.Error("failed to close LSM tree", slog.String("error", err.Error()))
+		}
+		os.Exit(0)
+	}()
+
+	// Start gRPC server
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		slog.Error("failed to listen", slog.String("error", err.Error()))
+		os.Exit(1)
 	}
-	s := grpc.NewServer()
-	pb.RegisterYakvServerServer(s, &server{})
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterYakvServerServer(grpcServer, &server{lsm: lsmTree})
 	slog.Info("server listening", slog.Any("address", lis.Addr()))
-	if err := s.Serve(lis); err != nil {
+
+	if err := grpcServer.Serve(lis); err != nil {
 		slog.Error("failed to serve", slog.String("error", err.Error()))
+		os.Exit(1)
 	}
 }
